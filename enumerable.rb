6b0e4832b8902d395d5c5823c1a1602642c09252
Marc raphael <marcraphael979@gmail.com>
2023-04-13 13:38:04 +0100

    Add enumerable

--- enumerable.rb
+++ enumerable.rb
@@ -1,1 +1,18 @@
-puts 'hello world'
+# Create a module MyEnumerable
+module MyEnumerable
+  def all?
+    each { |arg| return false unless yield(arg) }
+    true
+  end
+
+  def any?
+    each { |arg| return true if yield(arg) }
+    false
+  end
+
+  def filter?
+    res = []
+    each { |arg| res << arg if yield(arg) }
+    res
+  end
+end
